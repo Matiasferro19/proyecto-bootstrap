18b7ad849540557b8b85c18f18f6c6cb99ec4964
Matias <138623874+Matiasferro19@users.noreply.github.com>
2023-12-22 17:17:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const temaOscuro = () => {
+
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#icon").setAttribute("class" , "bi bi-sun-fill");
+
+
+}
+
+const temaClaro = () => {
+
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#icon").setAttribute("class", "bi bi-moon-fill");
+
+
+}
+
+
+const cambiarTema = () => {
+
+    document.querySelector("body").getAttribute("data-bs-theme") === "light"?
+    temaOscuro() : temaClaro();
+
+
+}
